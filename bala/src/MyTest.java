b8c69c69af2055557c4c01d5d5862abd7b165002
bala <bala@DESKTOP-G49UBE6>
2018-09-07 20:47:43 +0530

    hi

---
+++ bala/src/MyTest.java
@@ -0,0 +1,18 @@
+public class MyTest {
+	
+	public static void main(String[] args)
+    {
+      double d = 100.04;  
+      long l = (long)d;  
+      int i = (int)l;	 
+      int j[] ={1,2,3,4,5,6,7,8,9};
+      
+      
+      System.out.println("Double value "+d);
+      System.out.println("Long value "+l);
+      System.out.println("Int value "+i);
+      System.out.println(j[3]);
+      
+     
+    }
+}
